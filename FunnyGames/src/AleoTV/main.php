772d6df53dba34a80b69f3cddbd15806f7d21f88
AleoTV <53917303+AleoTV@users.noreply.github.com>
2020-02-13 14:20:30 +0100

    END Update for main.php

--- FunnyGames/src/AleoTV/main.php
+++ FunnyGames/src/AleoTV/main.php
@@ -2,5 +2,119 @@
 
 namespace AleoTV;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat as TF;
+use pocketmine\event\Listener;
+ use pocketmine\plugin\PluginBase;
+ use pocketmine\command\Command;
+ use pocketmine\command\CommandSender;
+ use pocketmine\Player;
+
+
+ class Main extends PluginBase implements Listener {
+ 
+    public function onEnable()
+    {
+        $this->getLogger()->info("[FunnyGames] Aktiviert.");
+    }
+
+    public $fts;
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+        $fts = "[FunnyGames]";
+        $this->fts = $fts;
+
+        if ($cmd->getName() == "tag") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("time.command")) {
+                    $sender->getLevel()->setTime(6000);
+                    $sender->sendMessage("§§l§fFunnyGames §8§l> §rDu hast Tag gemacht");
+                    return true;
+                } else {
+                    $sender->sendMessage("§rDu hast keine Rechte diesen Befehl zu benutzen");
+                    return true;
+                }
+            }
+            return true;
+        }
+        if ($cmd->getName() == "nacht") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("time.command")) {
+                    $sender->getLevel()->setTime(16000);
+                    $sender->sendMessage("§l§fFunnyGames §8§l> §rDu hast Nacht gemacht");
+                    return true;
+                } else {
+                    $sender->sendMessage("§cDu hast keine Rechte diesen Befehl zu benutzen");
+                    return true;
+                }
+            }
+            return true;
+        }
+        if ($cmd->getName() == "gm0") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("gamemode.command")) {
+                    $sender->setGamemode(0);
+                    $sender->sendMessage("§l§fFunnyGames §8§l> §rDu bist nun im gm0");
+                } else {
+                    $sender->sendMessage("§cDu hast keine Rechte diesen Befehl zu benutzen");
+                }
+            }
+            return true;
+        }
+        if ($cmd->getName() == "gm1") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("gamemode.command")) {
+                    $sender->setGamemode(1);
+                    $sender->sendMessage("§l§fFunnyGames §8§l> §rDu bist nun im gm1");
+                } else {
+                    $sender->sendMessage("§cDu hast keine Rechte diesen Befehl zu benutzen");
+                }
+            }
+            return true;
+        }
+        if ($cmd->getName() == "gm2") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("gamemode.command")) {
+                    $sender->setGamemode(2);
+                    $sender->sendMessage("§l§fFunnyGames §8§l> §rDu bist nun im gm2");
+                } else {
+                    $sender->sendMessage("§cDu hast keine Rechte diesen Befehl zu benutzen");
+                }
+            }
+            return true;
+		}
+        if ($cmd->getName() == "heal") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("heal.command")) {
+                    $sender->setHealth(20);
+                    $sender->sendMessage("§l§fFunnyGames §8§l> §rDeine Herzen wurden gefüllt");
+                } else {
+                    $sender->sendMessage("§cDu hast keine Rechte diesen Befehl zu benutzen");
+                }
+            }
+            return true;
+        }
+        if ($cmd->getName() == "food") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("food.command")) {
+                    $sender->setFood(20);
+                    $sender->sendMessage("§l§fFunnyGames §8§l> §rDeine Hungersleiste wurde gefüllt");
+                } else {
+                    $sender->sendMessage("§cDu hast keine Rechte diesen Befehl zu benutzen");
+                }
+            }
+            return true;
+        }
+        if ($cmd->getName() == "gm3") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("gamemode.command")) {
+                    $sender->setGamemode(3);
+                    $sender->sendMessage("§l§fFunnyGames §8§l> §rDu bist nun im gm3");
+                } else {
+                    $sender->sendMessage("§cDu hast keine Rechte diesen Befehl zu benutzen");
+                }
+            }
+            return true;
+         }
+        return true;
+        }
+    }

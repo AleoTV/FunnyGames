ae0230bfdb41378a6675b0056d9ab17ec4402373
AleoTV <53917303+AleoTV@users.noreply.github.com>
2020-02-14 22:01:13 +0100

    update main.php

--- FunnyGames/src/AleoTV/main.php
+++ FunnyGames/src/AleoTV/main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AleoTV\FunnyGames;
+namespace AleoTV;
 
 use pocketmine\event\Listener;
  use pocketmine\plugin\PluginBase;
@@ -8,7 +8,7 @@ use pocketmine\event\Listener;
  use pocketmine\command\CommandSender;
  use pocketmine\Player;
 
-class FunnyGames extends PluginBase {
+class main extends PluginBase {
  
     public function onEnable()
     {
